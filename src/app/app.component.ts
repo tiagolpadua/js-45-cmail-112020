e314e6ae6b89117755ba4bbdf67bd2ba46832c93
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-08 21:47:40 -0300

    c10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   private _isNewEmailFormOpen = false;
+
+  emailList = [];
+
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  };
+
   get isNewEmailFormOpen(): boolean {
     return this._isNewEmailFormOpen;
   }
+
   toggleNewEmailForm(): void {
     this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
+
+  handleNewEmail(event: Event): void {
+    event.preventDefault();
+    this.emailList.push(this.email);
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    };
   }
 }
