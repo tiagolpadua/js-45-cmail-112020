ef2c5c81e9ec822fc924089bc6ec965af5c4afa9
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-09 21:56:34 -0300

    c15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
-
+import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
+import { LoginComponent } from './modules/login/login.component';
+import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import { ModuloRoteamento } from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CaixaDeEntradaComponent,
+    LoginComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ModuloRoteamento
   ],
   providers: [],
   bootstrap: [AppComponent]
