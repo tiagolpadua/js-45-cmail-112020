061134e0d101411abf82d2efa654bc06e3ce2872
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-11 23:18:27 -0300

    c18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ModuloRoteamento } from './app.routes';
+import { CmailFormFieldDirective } from './components/cmail-form-group/cmail-form-field.directive';
+import { CmailFormGroupComponent } from './components/cmail-form-group/cmail-form-group.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CadastroComponent } from './modules/cadastro/cadastro.component';
 import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
 import { LoginComponent } from './modules/login/login.component';
-import { CadastroComponent } from './modules/cadastro/cadastro.component';
-import { ModuloRoteamento } from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +16,8 @@ import { ModuloRoteamento } from './app.routes';
     CaixaDeEntradaComponent,
     LoginComponent,
     CadastroComponent,
+    CmailFormGroupComponent,
+    CmailFormFieldDirective
   ],
   imports: [
     BrowserModule,
