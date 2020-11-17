3f8a30b4c5a4a7effb8f50dd317ceb9d5124e699
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-16 23:52:03 -0300

    c35

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cmail-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  login = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  mensagemErro: string;
 
-  ngOnInit(): void {
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void { }
+
+  handleLogin(formLogin: NgForm): void {
+    if (formLogin.valid) {
+      this.httpClient
+        .post('http://localhost:3200/login', this.login)
+        .subscribe(
+          (response: any) => {
+            localStorage.setItem('cmail-token', response.token);
+          },
+          (responseError: HttpErrorResponse) => {
+            this.mensagemErro = responseError.error.body;
+          }
+        );
+    }
   }
-
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CMailFormModule } from 'src/app/components/cmail-form-group/cmail-form.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
     declarations: [LoginComponent],
-    imports: [CommonModule, LoginRoutingModule]
+    imports: [CommonModule, LoginRoutingModule, CMailFormModule, FormsModule, HttpClientModule]
 })
 
 export class LoginModule { }
