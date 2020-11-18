d9323973bb06ad64cdade698ab7c745a209ad2f2
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-11-17 23:17:15 -0300

    c42

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MensagemErroComponent } from './mensagem-erro/mensagem-erro.component';
+import { CmailListItemComponent } from './cmail-list-item/cmail-list-item.component';
 
 @NgModule({
-    declarations: [HeaderComponent, MensagemErroComponent],
+    declarations: [HeaderComponent, MensagemErroComponent, CmailListItemComponent],
     imports: [CommonModule, RouterModule],
-    exports: [HeaderComponent, MensagemErroComponent]
+    exports: [HeaderComponent, MensagemErroComponent, CmailListItemComponent]
 })
 
 export class SharedComponentModule { }
